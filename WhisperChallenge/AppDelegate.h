dd70bec512522f3211a0d56dad4469591e7edd39
Jackson Jessup <jessupjn@umich.edu>
2015-04-30 09:16:35 -0400

    Initial Commit

---
+++ WhisperChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WhisperChallenge
+//
+//  Created by Jack on 4/30/15.
+//  Copyright (c) 2015 Jackson Jessup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WhisperChallenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WhisperChallenge
+//
+//  Created by Jack on 4/30/15.
+//  Copyright (c) 2015 Jackson Jessup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
